dfa1c8b16cf8224f302e3b338e2776b220da5072
Deep642 <89216609+Deep642@users.noreply.github.com>
2023-01-25 19:09:27 +0530

    Update route.js

--- server/route.js
+++ server/route.js
@@ -1,12 +1,62 @@
-import express from 'express';
-import { getUsers, addUser, getUserById, editUser, deleteUser } from '../controller/user-controller.js';
+const User = require("../model/user");
+const router = require("express").Router();
 
-const router = express.Router();
 
-router.get('/', getUsers);
-router.post('/add', addUser);
-router.get('/:id', getUserById);
-router.put('/:id', editUser);
-router.delete('/:id', deleteUser);
 
-export default router;
+router.get('/', async (request, response) => {
+    try{
+        const users = await User.find();
+        response.status(200).json(users);
+    }catch( error ){
+        response.status(404).json({ message: error.message })
+    }
+});
+
+
+
+router.post('/add', async (request, response) => {
+    const user = request.body;
+    
+    const newUser = new User(user);
+    try{
+        await newUser.save();
+        response.status(201).json(newUser);
+    } catch (error){
+        response.status(409).json({ message: error.message});     
+    }
+});
+router.get('/:id', async (request, response) => {
+    try{
+        const user = await User.findById(request.params.id);
+        response.status(200).json(user);
+    }catch( error ){
+        response.status(404).json({ message: error.message })
+    }
+});
+
+
+router.put('/:id', async (request, response) => {
+    let user = request.body;
+
+    const editUser = new User(user);
+    try{
+        await User.updateOne({_id: request.params.id}, editUser);
+        response.status(201).json(editUser);
+    } catch (error){
+        response.status(409).json({ message: error.message});     
+    }
+});
+
+
+router.delete('/:id', async (request, response) => {
+    try{
+        await User.deleteOne({_id: request.params.id});
+        response.status(201).json("User deleted Successfully");
+    } catch (error){
+        response.status(409).json({ message: error.message});     
+    }
+});
+
+
+module.exports = router;
+
